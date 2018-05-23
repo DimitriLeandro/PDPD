0c8e25b010ada409ccba4555051730c6db6fbae4
Dimitri Leandro <dimitri.leandro@gmail.com>
2018-05-23 17:57:01 -0300

    Alterando organização do projeto para adicionar análise da Classe AB

---
+++ PDPD/src/javaframes/MainPDPD.java
@@ -0,0 +1,106 @@
+package javaframes;
+
+import javaframes.classea.MainClasseA;
+import javaframes.classeab.MainClasseAB;
+
+/**
+ *
+ * @author dimi
+ */
+public class MainPDPD extends javax.swing.JFrame {
+
+    MainClasseA objMainClasseA;
+    MainClasseAB objMainClasseAB;
+    
+    public MainPDPD() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        btnClasseA = new javax.swing.JButton();
+        btnClasseAB = new javax.swing.JButton();
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Análise de Amplificadores");
+
+        btnClasseA.setText("Análise Classe AB");
+        btnClasseA.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnClasseAActionPerformed(evt);
+            }
+        });
+
+        btnClasseAB.setText("Análise Classe A");
+        btnClasseAB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnClasseABActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(45, 45, 45)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnClasseA, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnClasseAB, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(44, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnClasseAB, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnClasseA, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnClasseABActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClasseABActionPerformed
+        objMainClasseA = new MainClasseA(this);
+        objMainClasseA.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_btnClasseABActionPerformed
+
+    private void btnClasseAActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClasseAActionPerformed
+        objMainClasseAB = new MainClasseAB(this);
+        objMainClasseAB.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_btnClasseAActionPerformed
+
+    public static void main(String args[]) {
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainPDPD().setVisible(true);
+            }
+        });
+    }
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnClasseA;
+    private javax.swing.JButton btnClasseAB;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ PDPD/src/javaframes/classeab/MainClasseAB.java
@@ -0,0 +1,63 @@
+package javaframes.classeab;
+
+import javaframes.MainPDPD;
+
+/**
+ *
+ * @author dimi
+ */
+public class MainClasseAB extends javax.swing.JFrame {
+
+    MainPDPD objMainFramePDPD;
+    
+    public MainClasseAB(MainPDPD obj) {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        objMainFramePDPD = obj;
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btnVoltar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Análise Classe AB");
+
+        btnVoltar.setText("Voltar");
+        btnVoltar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVoltarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(133, 133, 133)
+                .addComponent(btnVoltar)
+                .addContainerGap(213, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(216, Short.MAX_VALUE)
+                .addComponent(btnVoltar)
+                .addGap(55, 55, 55))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
+        objMainFramePDPD.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_btnVoltarActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnVoltar;
+    // End of variables declaration//GEN-END:variables
+}
